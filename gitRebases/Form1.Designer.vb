37f38a9fb827955e23396faaa3244c403db24b1a
wdhae <willem@tcmb.eu>
2011-11-15 12:48:21 +0100

    extra textbox

--- gitRebases/Form1.Designer.vb
+++ gitRebases/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button
         Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
         Me.SuspendLayout()
         '
         'Button1
@@ -37,17 +38,26 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 30)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 24)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(141, 58)
+        Me.TextBox1.Size = New System.Drawing.Size(90, 58)
         Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(130, 25)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(93, 58)
+        Me.TextBox2.TabIndex = 2
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
@@ -58,5 +68,6 @@ Partial Class Form1
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
 
 End Class
--- gitRebases/Form1.vb
+++ gitRebases/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        MessageBox.Show(String.Format("Hello, {0} !!", TextBox1.Text))
+        MessageBox.Show(String.Format("Hello, {0} , {1} !!", TextBox1.Text, TextBox2.Text()))
     End Sub
 End Class
