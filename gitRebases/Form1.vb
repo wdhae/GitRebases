1f5acc4c83e760e81314cc0681cd7a88f008de31
wdhae <willem@tcmb.eu>
2011-11-15 11:48:16 +0100

    added hello

--- gitRebases/Form1.vb
+++ gitRebases/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show(String.Format("Hello, {0} !!", TextBox1.Text))
+    End Sub
 End Class
